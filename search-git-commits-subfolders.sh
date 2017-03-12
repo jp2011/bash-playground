258d0d3518a351a83122edd9f6d72781d8b7c9a0
Jan <jan.povala@gmail.com>
2017-03-12 21:48:02 +0000

    Removing unnecessary code

--- search-git-commits-subfolders.sh
+++ search-git-commits-subfolders.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
 BOLD="$(tput bold)"
-STANDOUT="$(tput smso)"
-CLR="$(tput sgr0)"
-BLACK="$(tput setaf 0)"
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-YELLOW="$(tput setaf 3)"
-BLUE="$(tput setaf 4)"
-MAGENTA="$(tput setaf 5)"
-CYAN="$(tput setaf 6)"
-WHITE="$(tput setaf 7)"
-
-
-# set -o pipefail  # this makes sure that if any of the commands in the pipeline fail, that error code will be returned
 
 for d in ./*/ ; do
 	if ( cd "$d" && git log --oneline | grep "$1" ) ; then
